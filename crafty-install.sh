be41a217f3f26eb976d9a419e0725b179eb8721e
CraftyRobott <jmhthethird10@gmail.com>
2026-05-02 10:49:23 -0400

    test 4

--- crafty-install.sh
+++ crafty-install.sh
@@ -1,5 +1,4 @@
-pacman -S --noconfirm hyprland ly kitty
+pacman -S --noconfirm hyprland ly kitty mesa vulkan-radeon intel-media-driver libva-intel-driver vulkan-intel vulkan-nouveau xf86-video-amdgpu xf86-video-ati xf86-video-nouveau
 systemctl enable ly@tty2.service
 mkdir ~/.config/hypr
 mv ~/Crafty-installer/hyprland.conf ~/.config/hypr/
-reboot
